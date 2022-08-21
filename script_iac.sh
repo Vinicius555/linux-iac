9355f098ddd381308adc787b76ddc0562539c689
Vinicius555 <107134566+Vinicius555@users.noreply.github.com>
2022-08-21 12:08:50 -0300

    Add files via upload

---
+++ script_iac.sh
@@ -0,0 +1,17 @@
+  GNU nano 6.2        script.iac2.sh                  
+#!/bin/bash
+
+echo "Atualizando o Servidor...."
+
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+
+echo "Baixando e copiando os arquivos da aplicação..."
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-di>
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
+
